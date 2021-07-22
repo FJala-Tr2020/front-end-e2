1e96b3385be3c7a6789fa9c9988109ae4f297f61
kleyla <kleyla01@live.com>
2021-07-22 10:41:00 -0400

    :hammer: deleting header from axios

--- src/config/AxiosInstance.ts
+++ src/config/AxiosInstance.ts
@@ -2,9 +2,6 @@ import axios from 'axios';
 
 const AxiosInstance = axios.create({
   baseURL: process.env.BASE_URL,
-  headers: {
-    'Access-Control-Allow-Origin': '*'
-  }
 });
 
 export default AxiosInstance;
