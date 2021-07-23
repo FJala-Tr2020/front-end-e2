06855fac6d1dd8dd05c30fcdfeb6dead40d6a2bf
kleyla <kleyla01@live.com>
2021-07-23 11:28:09 -0400

    :100: adding test for Leads class

--- src/components/leads/Leads.ts
+++ src/components/leads/Leads.ts
@@ -1,28 +1,4 @@
-import * as ko from 'knockout';
 import template from './Leads.html';
-import { RequestLead } from '../../helpers/request';
-
-class Leads {
-  request: RequestLead;
-  leadsList = ko.observableArray();
-  description;
-
-  constructor() {
-    this.request = new RequestLead();
-    this.loadLeads();
-    this.description = process.env.DESCRIPTION_TABLE_LEADS;
-  }
-
-  loadLeads(): void {
-    this.request
-      .getLeads()
-      .then((response) => {
-        this.leadsList(response);
-      })
-      .catch((error) => {
-        throw new Error(error);
-      });
-  }
-}
+import { Leads } from './Leads.class';
 
 export default { viewModel: Leads, template: template };
